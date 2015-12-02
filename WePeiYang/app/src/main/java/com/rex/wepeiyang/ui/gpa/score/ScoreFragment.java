1e5c340ee3ef073cddf544e3b48573684ecb2546
孙俊韬 <sunjuntao123@gmail.com>
2015-12-02 10:19:38 +0800

    basic part of mpandroidchart finish

--- WePeiYang/app/src/main/java/com/rex/wepeiyang/ui/gpa/score/ScoreFragment.java
+++ WePeiYang/app/src/main/java/com/rex/wepeiyang/ui/gpa/score/ScoreFragment.java
@@ -1,5 +1,6 @@
 package com.rex.wepeiyang.ui.gpa.score;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
@@ -12,10 +13,21 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ValueFormatter;
 import com.rex.wepeiyang.R;
 import com.rex.wepeiyang.bean.Gpa;
 import com.rex.wepeiyang.ui.BaseFragment;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,7 +37,7 @@ import butterknife.InjectView;
 /**
  * Created by sunjuntao on 15/11/15.
  */
-public class ScoreFragment extends BaseFragment implements ScoreView, View.OnClickListener {
+public class ScoreFragment extends BaseFragment implements ScoreView, View.OnClickListener, OnChartValueSelectedListener {
     @InjectView(R.id.line_chart)
     LineChart lineChart;
     @InjectView(R.id.tv_zongjiaquan)
@@ -65,11 +77,71 @@ public class ScoreFragment extends BaseFragment implements ScoreView, View.OnCli
 
     @Override
     public void bindData(Gpa gpa) {
+        final DecimalFormat decimalFormat = new DecimalFormat("###.00");
         tvZongjiaquan.setText(gpa.data.stat.total.score + "");
-        tvZongjidian.setText(gpa.data.stat.total.gpa + "");
+        tvZongjidian.setText(decimalFormat.format(gpa.data.stat.total.gpa));
         courses.clear();
         courses.addAll(gpa.data.data.get(gpa.data.data.size() - 1).data);
         adapter.refreshItemsByScore(courses);
+        lineChart.setOnChartValueSelectedListener(this);
+        lineChart.setNoDataTextDescription("还没有成绩哟");
+        lineChart.setDrawGridBackground(false);
+        lineChart.setDragEnabled(false);
+        lineChart.animateX(1000);
+        lineChart.setDrawBorders(false);
+        lineChart.setDescription("");
+        lineChart.setDoubleTapToZoomEnabled(false);
+        lineChart.setPinchZoom(true);
+        lineChart.setAutoScaleMinMaxEnabled(true);
+        lineChart.setGridBackgroundColor(Color.BLACK);
+        lineChart.setBorderWidth(3);
+        lineChart.setDrawBorders(false);
+        lineChart.getLegend().setEnabled(false);
+        XAxis xAxis = lineChart.getXAxis();
+        xAxis.setDrawAxisLine(false);
+        xAxis.setDrawGridLines(false);
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setTextColor(ContextCompat.getColor(getActivity(), R.color.text_secondary_color));
+        xAxis.setDrawLabels(true);
+        xAxis.setTextSize(5);
+        YAxis yAxis = lineChart.getAxisLeft();
+        yAxis.setAxisMaxValue(4f);
+        yAxis.setStartAtZero(false);
+        yAxis.setDrawAxisLine(false);
+        yAxis.setDrawGridLines(false);
+        yAxis.setDrawLabels(false);
+        //yAxis.setLabelCount(4, true);
+        YAxis rightAxis = lineChart.getAxisRight();
+        rightAxis.setDrawLabels(false);
+        rightAxis.setDrawAxisLine(false);
+        rightAxis.setDrawGridLines(false);
+
+
+        ArrayList<String> xVals = new ArrayList<>();
+        for (int i = 0; i < gpa.data.data.size(); i++) {
+            xVals.add(gpa.data.data.get(i).name);
+        }
+        ArrayList<Entry> yVals = new ArrayList<>();
+        for (int i = 0; i < gpa.data.data.size(); i++) {
+            double val = gpa.data.data.get(i).stat.gpa;
+            yVals.add(new Entry((float)val, i));
+        }
+        LineDataSet set = new LineDataSet(yVals, null);
+        set.setCircleColor(ContextCompat.getColor(getActivity(), R.color.text_secondary_color));
+        set.setCircleSize(4);
+        set.setValueFormatter(new ValueFormatter() {
+            @Override
+            public String getFormattedValue(float value) {
+                return decimalFormat.format(value);
+            }
+        });
+        set.setDrawHighlightIndicators(false);
+        set.setValueTextSize(10);
+        set.setLineWidth(1);
+        set.setColor(ContextCompat.getColor(getActivity(), R.color.text_secondary_color));
+        set.setValueTextColor(ContextCompat.getColor(getActivity(), R.color.text_secondary_color));
+        LineData data = new LineData(xVals, set);
+        lineChart.setData(data);
     }
 
     @Override
@@ -95,5 +167,15 @@ public class ScoreFragment extends BaseFragment implements ScoreView, View.OnCli
                     btnOrderByScore.setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.gpa_primary_color));
                 }
         }
+    }
+
+    @Override
+    public void onValueSelected(Entry e, int dataSetIndex, Highlight h) {
+
+    }
+
+    @Override
+    public void onNothingSelected() {
+
     }
 }
