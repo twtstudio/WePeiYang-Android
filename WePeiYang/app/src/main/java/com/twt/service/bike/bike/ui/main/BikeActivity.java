3b28ed0a309e4403626fcba59d0b6f879d8318e8
life2015 <976885345@qq.com>
2016-08-20 10:51:27 +0800

    able to compile

---
+++ WePeiYang/app/src/main/java/com/twt/service/bike/bike/ui/main/BikeActivity.java
@@ -0,0 +1,10 @@
+package com.twt.service.bike.bike.ui.main;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by root on 16-8-20.
+ */
+public class BikeActivity extends AppCompatActivity {
+
+}
