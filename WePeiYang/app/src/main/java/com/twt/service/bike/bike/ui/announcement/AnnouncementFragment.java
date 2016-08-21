24c12e011ccd5e4301aeb422d2c67ba3b3fb0a2f
life2015 <976885345@qq.com>
2016-08-21 12:58:05 +0800

    announcement adapter added...ok

--- WePeiYang/app/src/main/java/com/twt/service/bike/bike/ui/announcement/AnnouncementFragment.java
+++ WePeiYang/app/src/main/java/com/twt/service/bike/bike/ui/announcement/AnnouncementFragment.java
@@ -1,10 +1,14 @@
 package com.twt.service.bike.bike.ui.announcement;
 
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import com.twt.service.R;
 import com.twt.service.bike.common.ui.PFragment;
+import com.twt.service.bike.model.BikeAnnouncement;
+
+import java.util.List;
 
 import butterknife.InjectView;
 
@@ -16,6 +20,7 @@ public class AnnouncementFragment extends PFragment<AnnouncementPresenter> imple
     RecyclerView mRecyclerView;
     @InjectView(R.id.bike_announcement_srl)
     SwipeRefreshLayout mSwipeRefreshLayout;
+    private AnnouncementAdapter mAdapter;
 
     @Override
     protected AnnouncementPresenter getPresenter() {
@@ -29,6 +34,17 @@ public class AnnouncementFragment extends PFragment<AnnouncementPresenter> imple
 
     @Override
     protected void initView() {
+        mAdapter = new AnnouncementAdapter(getContext());
+        mSwipeRefreshLayout.setOnRefreshListener(() -> mPresenter.getAnnouncement());
+        mSwipeRefreshLayout.post(() -> mPresenter.getAnnouncement());
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        mAdapter.showFooter();
+        mRecyclerView.setAdapter(mAdapter);
+    }
 
+    @Override
+    public void setAnnouncementList(List<BikeAnnouncement> announcementList) {
+        mSwipeRefreshLayout.setRefreshing(false);
+        mAdapter.refreshItems(announcementList);
     }
 }
