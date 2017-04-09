3c7ed34f261ddb0c454558f7666e8abfdca53359
retrox <976885345@qq.com>
2017-04-09 16:33:22 +0800

    kotlin测试白盒，添加图集功能中

---
+++ WePeiYang/news/src/main/java/com/twtstudio/retrox/news/api/GalleryIndexBean.java
@@ -0,0 +1,28 @@
+package com.twtstudio.retrox.news.api;
+
+/**
+ * Created by retrox on 09/04/2017.
+ */
+
+public class GalleryIndexBean {
+
+    /**
+     * id : 45
+     * title : 愿与时光温柔以待
+     * description :
+     * author :
+     * coverUrl : http://www.twt.edu.cn/upload/galleries/7Wyf5OMa8faeGRExrksw.jpeg
+     * view_count : 15823
+     * coverThumbnailUrl : http://www.twt.edu.cn/upload/galleries/7Wyf5OMa8faeGRExrksw.jpeg
+     * dateTime : 2016-11-21T10:03:54+0800
+     */
+
+    public int id;
+    public String title;
+    public String description;
+    public String author;
+    public String coverUrl;
+    public int view_count;
+    public String coverThumbnailUrl;
+    public String dateTime;
+}
---
+++ WePeiYang/news/src/main/java/com/twtstudio/retrox/news/api/PicApi.kt
@@ -0,0 +1,36 @@
+package com.twtstudio.retrox.news.api
+
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Path
+import retrofit2.http.Query
+import rx.Observable
+
+/**
+ * Created by retrox on 09/04/2017.
+ */
+
+interface PicApi {
+
+    /**
+     * 方寸流年API
+     * @id 类型 随身拍：1 单反：2
+     * @from 从哪个id开始降序排列
+     */
+    @Headers("Accept: application/json")
+    @GET("https://photograph.twtstudio.com/list/{id}")
+    fun getFangcunPic(@Path("id") id: Int = 1, @Query("from") from: Int = 0): Observable<FangcunBean>
+
+    /**
+     * 主站图解获取索引的API
+     */
+    @GET("http://www.twt.edu.cn/mapi/galleries/index")
+    fun getGalleryIndex(): Observable<List<GalleryIndexBean>>
+
+    /**
+     * 主站图解索引内部组图API
+     */
+    @GET("http://www.twt.edu.cn/mapi/galleries/{id}/photos")
+    fun getGalleryPhotos(@Path("id") id: Int ): Observable<List<GalleryPhotoBean>>
+
+}
