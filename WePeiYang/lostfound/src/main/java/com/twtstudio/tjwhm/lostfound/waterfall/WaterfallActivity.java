676931efc149d1e24defa46b98296bc5c8857e53
tjwhm <759361109@qq.com>
2017-07-04 21:17:07 +0800

    added and changed some UI(enough for today)

--- WePeiYang/lostfound/src/main/java/com/twtstudio/tjwhm/lostfound/waterfall/WaterfallActivity.java
+++ WePeiYang/lostfound/src/main/java/com/twtstudio/tjwhm/lostfound/waterfall/WaterfallActivity.java
@@ -3,10 +3,12 @@ package com.twtstudio.tjwhm.lostfound.waterfall;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.twtstudio.tjwhm.lostfound.R;
@@ -83,6 +85,7 @@ public class WaterfallActivity extends BaseActivity {
         waterfall_tabLayout.setupWithViewPager(waterfall_pager);
         waterfall_tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
         waterfall_tabLayout.setSelectedTabIndicatorColor(Color.parseColor("#00a1e9"));
+
 
     }
 }
