c49aef30322ae5ca83b3ddd0fb6c7219068d3d94
CodeChild <zhangyulong@tju.edu.cn>
2017-07-09 19:02:02 +0800

    添加对课程起止周数的检查

--- WePeiYang/schedule/src/main/java/com/twtstudio/retrox/schedule/view/CourseIsEmptyViewModel.java
+++ WePeiYang/schedule/src/main/java/com/twtstudio/retrox/schedule/view/CourseIsEmptyViewModel.java
@@ -14,14 +14,14 @@ public class CourseIsEmptyViewModel implements ViewModel {
     private String day;
 
     CourseIsEmptyViewModel(CalendarDay calendarDay) {
-        if ((calendarDay.getMonth() >= 2 && calendarDay.getMonth() <6)
-                || (calendarDay.getMonth() >= 8 || calendarDay.getMonth() < 1))
-            text.set("暂时无法获取此学期的课表");
-        else {
+//        if ((calendarDay.getMonth() >= 2 && calendarDay.getMonth() <5)
+//                || (calendarDay.getMonth() >= 8 || calendarDay.getMonth() < 1))
+//            text.set("暂时无法获取此学期的课表");
+//        else {
             if (calendarDay.equals(CalendarDay.today()))
                 day = "今日";
             else day = "该日";
             text.set(day + "无课，做点有意思的事吧~");
-        }
+//        }
     }
 }
