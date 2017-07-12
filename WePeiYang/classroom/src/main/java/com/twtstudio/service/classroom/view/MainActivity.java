ecd78c33823a2c9bb99444633dcd235b72f50dce
CodeChild <zhangyulong@tju.edu.cn>
2017-07-12 15:10:22 +0800

    *

--- WePeiYang/classroom/src/main/java/com/twtstudio/service/classroom/view/MainActivity.java
+++ WePeiYang/classroom/src/main/java/com/twtstudio/service/classroom/view/MainActivity.java
@@ -8,15 +8,15 @@ import com.alibaba.android.arouter.facade.annotation.Route;
 import com.trello.rxlifecycle.components.support.RxAppCompatActivity;
 import com.twt.wepeiyang.commons.utils.CommonPrefUtil;
 import com.twtstudio.service.classroom.R;
-import com.twtstudio.service.classroom.databinding.ActivityMainBinding;
+import com.twtstudio.service.classroom.databinding.ActivityClassroomQueryMainBinding;
 @Route(path = "/classroom/main")
 public class MainActivity extends RxAppCompatActivity {
-    ActivityMainBinding mainBinding;
+    ActivityClassroomQueryMainBinding mainBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mainBinding= DataBindingUtil.setContentView(this, R.layout.activity_main);
+        setContentView(R.layout.activity_classroom_query_main);
+        mainBinding= DataBindingUtil.setContentView(this, R.layout.activity_classroom_query_main);
         MainActivityViewModel viewModel=new MainActivityViewModel(this,46,21,3, CommonPrefUtil.getStudentNumber());
         mainBinding.setViewModel(viewModel);
 
