dd307bf62f802bd49fe504320b8296e07784064e
CodeChild <zhangyulong@tju.edu.cn>
2017-07-12 15:10:22 +0800

    *

--- WePeiYang/app/src/main/java/com/twt/service/MainActivity.java
+++ WePeiYang/app/src/main/java/com/twt/service/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_classroom_query_main);
 
 
         mButton = (Button) findViewById(R.id.button);
