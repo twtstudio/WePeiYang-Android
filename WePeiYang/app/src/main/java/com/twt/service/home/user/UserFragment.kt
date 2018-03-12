7ea4b4a58e09fc2131be20231bc0561e7fa99226
rickygao <rckgao@gmail.com>
2018-03-13 00:03:08 +0800

    [fix] import

--- WePeiYang/app/src/main/java/com/twt/service/home/user/UserFragment.kt
+++ WePeiYang/app/src/main/java/com/twt/service/home/user/UserFragment.kt
@@ -12,6 +12,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import com.twt.service.R
 import com.twt.service.base.BaseFragment
 import com.twt.service.settings.SettingsActivity
 import com.twt.wepeiyang.commons.cache.CacheProvider
