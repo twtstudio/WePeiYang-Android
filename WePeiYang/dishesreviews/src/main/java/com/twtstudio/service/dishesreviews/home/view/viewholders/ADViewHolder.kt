969052866d555d34b541a474656423388821d1be
CodeChild <zhangyulong1997@hotmail.com>
2018-05-02 21:26:08 +0800

    [add] 在主页添加banner

--- WePeiYang/dishesreviews/src/main/java/com/twtstudio/service/dishesreviews/home/view/viewholders/ADViewHolder.kt
+++ WePeiYang/dishesreviews/src/main/java/com/twtstudio/service/dishesreviews/home/view/viewholders/ADViewHolder.kt
@@ -1,20 +1,38 @@
 package com.twtstudio.service.dishesreviews.home.view.viewholders
 
 import android.arch.lifecycle.LifecycleOwner
-import android.arch.lifecycle.ViewModel
 import android.view.View
+import com.twtstudio.service.dishesreviews.R
 import com.twtstudio.service.dishesreviews.base.BaseItemViewHolder
+import com.twtstudio.service.dishesreviews.home.model.BannerImageLoader
+import com.youth.banner.Banner
+import com.youth.banner.BannerConfig
 
 /**
  * Created by zhangyulong on 18-3-23.
  */
 class ADViewHolder(itemView: View, lifecycleOwner: LifecycleOwner) : BaseItemViewHolder(itemView, lifecycleOwner) {
-    override fun bind() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    private val IMAGE_NUMBER = 3
+    private val BANNER_DELAY_TIME = 5000
+    private val banner = itemView.findViewById<Banner>(R.id.banner_ad)
+    private val images = arrayOfNulls<String>(IMAGE_NUMBER)
+    private val testImages = arrayOf(R.drawable.dishes_reviews_ju, R.drawable.dishes_reviews_lan, R.drawable.dishes_reviews_zhu)
+
+    init {
+        banner.apply {
+            setImageLoader(BannerImageLoader())
+            setImages(testImages.toMutableList())
+            setIndicatorGravity(BannerConfig.CENTER)
+            setOnBannerListener {
+
+            }
+            setDelayTime(BANNER_DELAY_TIME)
+            start()
+        }
     }
 
-    override fun bind(viewModel: ViewModel) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun bind() {
+
     }
 
 }
