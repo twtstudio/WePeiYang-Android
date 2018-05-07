04e2d909cd413a647a3e13ba2db3d82c93edeeb3
CodeChild <zhangyulong1997@hotmail.com>
2018-05-07 19:50:50 +0800

    [add]搜索页添加“添加菜品”button

--- WePeiYang/dishesreviews/src/main/java/com/twtstudio/service/dishesreviews/home/view/viewholders/ReviewViewHolder.kt
+++ WePeiYang/dishesreviews/src/main/java/com/twtstudio/service/dishesreviews/home/view/viewholders/ReviewViewHolder.kt
@@ -2,7 +2,16 @@ package com.twtstudio.service.dishesreviews.home.view.viewholders
 
 import android.arch.lifecycle.LifecycleOwner
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import com.twtstudio.service.dishesreviews.R
 import com.twtstudio.service.dishesreviews.base.BaseItemViewHolder
 
 class ReviewViewHolder(itemView: View, lifecycleOwner: LifecycleOwner) : BaseItemViewHolder(itemView, lifecycleOwner) {
+    private val ivDish = itemView.findViewById<ImageView>(R.id.iv_dish)
+    private val tvDishName = itemView.findViewById<TextView>(R.id.tv_dish_name)
+    private val tvUserName = itemView.findViewById<TextView>(R.id.tv_user_name)
+    private val tvDate = itemView.findViewById<TextView>(R.id.tv_date)
+    private val tvComment = itemView.findViewById<TextView>(R.id.tv_comment)
+
 }
