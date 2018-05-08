b93790e538741d617a735f3313377d43325a2f38
CodeChild <zhangyulong1997@hotmail.com>
2018-05-08 16:42:40 +0800

    [add]添加菜品页

---
+++ WePeiYang/dishesreviews/src/main/java/com/twtstudio/service/dishesreviews/dish/view/DishActivity.kt
@@ -0,0 +1,12 @@
+package com.twtstudio.service.dishesreviews.dish.view
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.twtstudio.service.dishesreviews.R
+
+class DishActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dishes_reviews_activity_dish)
+    }
+}
---
+++ WePeiYang/dishesreviews/src/main/java/com/twtstudio/service/dishesreviews/dish/view/adapters/LabelAdapter.kt
@@ -0,0 +1,17 @@
+package com.twtstudio.service.dishesreviews.dish.view.adapters
+
+import android.arch.lifecycle.LifecycleOwner
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import com.twtstudio.service.dishesreviews.base.BaseMutableListAdapter
+
+class LabelAdapter(list: MutableList<Any>, context: Context, owner: LifecycleOwner) : BaseMutableListAdapter(list, context, owner) {
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): RecyclerView.ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder?, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
