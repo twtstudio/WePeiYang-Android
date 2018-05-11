913cd79fa851ba46e536315bdc5395452014428e
CodeChild <zhangyulong1997@hotmail.com>
2018-05-12 00:46:06 +0800

    [mod]调整菜品页布局

--- WePeiYang/dishesreviews/src/main/java/com/twtstudio/service/dishesreviews/dish/view/adapters/CommentAdapter.kt
+++ WePeiYang/dishesreviews/src/main/java/com/twtstudio/service/dishesreviews/dish/view/adapters/CommentAdapter.kt
@@ -4,14 +4,16 @@ import android.arch.lifecycle.LifecycleOwner
 import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.ViewGroup
+import com.twtstudio.service.dishesreviews.R
 import com.twtstudio.service.dishesreviews.base.BaseListAdapter
+import com.twtstudio.service.dishesreviews.dish.view.viewholders.CommentViewHolder
 
 class CommentAdapter(list: List<Any>, context: Context, owner: LifecycleOwner) : BaseListAdapter(list, context, owner) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return CommentViewHolder(inflater.inflate(R.layout.dishes_reviews_item_dish_comment, parent, false), owner)
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 }
