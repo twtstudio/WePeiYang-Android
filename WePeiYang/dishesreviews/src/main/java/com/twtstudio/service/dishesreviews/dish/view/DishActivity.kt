eb72b434a4e60cd0abc6811e5fb519ff2953a51f
CodeChild <zhangyulong1997@hotmail.com>
2018-05-12 00:46:06 +0800

    [mod]调整菜品页布局

--- WePeiYang/dishesreviews/src/main/java/com/twtstudio/service/dishesreviews/dish/view/DishActivity.kt
+++ WePeiYang/dishesreviews/src/main/java/com/twtstudio/service/dishesreviews/dish/view/DishActivity.kt
@@ -2,11 +2,46 @@ package com.twtstudio.service.dishesreviews.dish.view
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
+import android.widget.TextView
 import com.twtstudio.service.dishesreviews.R
 
 class DishActivity : AppCompatActivity() {
+    private lateinit var toolbar: Toolbar
+    private lateinit var tvTitle: TextView
+    private lateinit var rvLabel: RecyclerView
+    private lateinit var rvComment: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dishes_reviews_activity_dish)
+        toolbar = findViewById<Toolbar>(R.id.toolbar).apply {
+            title = ""
+        }
+        setSupportActionBar(toolbar)
+        getSupportActionBar()?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.dishes_reviews_ic_action_back)
+        }
+        tvTitle = findViewById<TextView>(R.id.tv_toolbar_title).apply {
+
+        }
+        rvLabel = findViewById<RecyclerView>(R.id.rv_labels).apply {
+            layoutManager = GridLayoutManager(this@DishActivity, 2)
+        }
+        rvComment = findViewById<RecyclerView>(R.id.rv_comment).apply {
+            layoutManager = LinearLayoutManager(this@DishActivity, LinearLayoutManager.VERTICAL, false)
+        }
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- WePeiYang/dishesreviews/src/main/java/com/twtstudio/service/dishesreviews/dish/view/adapters/LabelAdapter.kt
+++ WePeiYang/dishesreviews/src/main/java/com/twtstudio/service/dishesreviews/dish/view/adapters/LabelAdapter.kt
@@ -4,14 +4,16 @@ import android.arch.lifecycle.LifecycleOwner
 import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.ViewGroup
+import com.twtstudio.service.dishesreviews.R
 import com.twtstudio.service.dishesreviews.base.BaseMutableListAdapter
+import com.twtstudio.service.dishesreviews.dish.view.viewholders.LabelViewHolder
 
 class LabelAdapter(list: MutableList<Any>, context: Context, owner: LifecycleOwner) : BaseMutableListAdapter(list, context, owner) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return LabelViewHolder(inflater.inflate(R.layout.dishes_reviews_item_dish_label, parent, false), owner)
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 }
