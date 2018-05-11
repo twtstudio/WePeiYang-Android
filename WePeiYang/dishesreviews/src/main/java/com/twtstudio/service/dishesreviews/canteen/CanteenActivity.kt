aeda8feb40f472f7a99fabc096079ff80ecb79fd
SG-XM <32589854+SG-XM@users.noreply.github.com>
2018-05-11 21:38:57 +0800

    [add]首页到餐厅页跳转

--- WePeiYang/dishesreviews/src/main/java/com/twtstudio/service/dishesreviews/canteen/CanteenActivity.kt
+++ WePeiYang/dishesreviews/src/main/java/com/twtstudio/service/dishesreviews/canteen/CanteenActivity.kt
@@ -6,6 +6,7 @@ import android.support.v4.app.FragmentPagerAdapter
 import android.support.v7.app.AppCompatActivity
 import com.twtstudio.service.dishesreviews.R
 import kotlinx.android.synthetic.main.dishes_reviews_activity_canteen.*
+import kotlinx.android.synthetic.main.dishes_reviews_toolbar.*
 
 class CanteenActivity : AppCompatActivity() {
 
@@ -18,11 +19,13 @@ class CanteenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dishes_reviews_activity_canteen)
+        tv_toolbar_title.text = intent.getStringExtra("CanteenName")
         fragments.add(CanteenFragment())
         vp_stairs.adapter = fpAdapter
-        tab_stairs.setupWithViewPager(vp_stairs)//这里面有代码会removeAllTabs，必须放前面
-        tab_stairs.removeAllTabs()
-        tab_stairs.addTab(tab_stairs.newTab().apply { text = "一层" })
-
+        tab_stairs.apply {
+            setupWithViewPager(vp_stairs)//这里面有代码会removeAllTabs，必须放前面
+            removeAllTabs()
+            addTab(tab_stairs.newTab().apply { text = "一层" })
+        }
     }
 }
