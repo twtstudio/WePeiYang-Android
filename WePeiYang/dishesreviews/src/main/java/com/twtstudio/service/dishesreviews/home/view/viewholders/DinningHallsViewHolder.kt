2bae395e5cb90bc64d695b14f5e7d4918d06f26e
CodeChild <zhangyulong1997@hotmail.com>
2018-05-12 20:13:12 +0800

    [mod]一些调整

--- WePeiYang/dishesreviews/src/main/java/com/twtstudio/service/dishesreviews/home/view/viewholders/DinningHallsViewHolder.kt
+++ WePeiYang/dishesreviews/src/main/java/com/twtstudio/service/dishesreviews/home/view/viewholders/DinningHallsViewHolder.kt
@@ -1,10 +1,11 @@
 package com.twtstudio.service.dishesreviews.home.view.viewholders
 
 import android.arch.lifecycle.LifecycleOwner
-import android.arch.lifecycle.ViewModel
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.twtstudio.service.dishesreviews.R
 import com.twtstudio.service.dishesreviews.base.BaseItemViewHolder
 import com.twtstudio.service.dishesreviews.home.view.adapters.DinningHallsAdapter
@@ -14,16 +15,19 @@ import com.twtstudio.service.dishesreviews.home.view.adapters.DinningHallsAdapte
  */
 class DinningHallsViewHolder(itemView: View, lifecycleOwner: LifecycleOwner) : BaseItemViewHolder(itemView, lifecycleOwner) {
     private val recyclerView = itemView.findViewById<RecyclerView>(R.id.rv_dinning_halls)
-
-
-    override fun bind(viewModel: ViewModel) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    private val spCampus = itemView.findViewById<Spinner>(R.id.sp_campus)
 
     override fun bind() {
         recyclerView.apply() {
             layoutManager = GridLayoutManager(itemView.context, 4)
             adapter = DinningHallsAdapter(emptyList(),itemView.context, lifecycleOwner)
+        }
+        spCampus.apply {
+            adapter = ArrayAdapter.createFromResource(itemView.context,
+                    R.array.campus_array, android.R.layout.simple_spinner_item)
+                    .apply {
+                        setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                    }
         }
     }
 }
