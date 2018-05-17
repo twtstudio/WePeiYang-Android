97137126e86a4194a598fd51cc9e780ff684f751
SG-XM <32589854+SG-XM@users.noreply.github.com>
2018-05-17 19:19:56 +0800

    [mod]食堂页优化

--- WePeiYang/dishesreviews/src/main/java/com/twtstudio/service/dishesreviews/canteen/Extentions.kt
+++ WePeiYang/dishesreviews/src/main/java/com/twtstudio/service/dishesreviews/canteen/Extentions.kt
@@ -1,7 +1,9 @@
 package com.twtstudio.service.dishesreviews.canteen
 
+import android.support.design.widget.TabLayout
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.widget.LinearLayout
 
 /**
  * Created by SGXM on 2018/5/6.
@@ -14,4 +16,21 @@ val recmove = fun RecyclerView.(n: Int) {
 //        this.scrollBy(0, view.top)
 //    }
     //layoutManager.scrollToPositionWithOffset(n, 0)
+}
+
+//通过反射修改TabLayout Indicator长度
+fun TabLayout.setIndicator(left: Int = 0, right: Int = 0) {
+    var clazz = this.javaClass
+    val tabStrip = clazz.getDeclaredField("mTabStrip")//获取Strip 返回Field类型
+    tabStrip.isAccessible = true
+    val linearTab = tabStrip.get(this) as LinearLayout//返回指定对象上由此Field表示的字段的值
+    for (i in 0 until linearTab.childCount) {
+        val child = linearTab.getChildAt(i)
+        child.isClickable = false
+        val params = child.layoutParams as LinearLayout.LayoutParams
+        params.leftMargin = left
+        params.rightMargin = right
+        child.requestLayout()
+        child.width
+    }
 }
