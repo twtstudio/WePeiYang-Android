a3929c7bb3796c5cdaa71d209f545e95cffdc3ec
WingedVampires <lsc990312@tju.edu.cn>
2018-09-06 16:39:13 +0800

    可以发送pic[],但是不能编辑

---
+++ WePeiYang/lostfound2/src/main/java/com/example/lostfond2/waterfall/WaterfallFilterTableAdapter.kt
@@ -0,0 +1,86 @@
+package com.example.lostfond2.waterfall
+
+import android.graphics.drawable.BitmapDrawable
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.PopupWindow
+import android.widget.TextView
+import android.widget.Toast
+import com.example.lostfond2.R
+import com.example.lostfond2.service.Utils
+import kotlinx.android.synthetic.main.activity_water_fall.*
+import android.view.Gravity
+
+
+class WaterfallFilterTableAdapter(val waterfallActivity: WaterFallActivity,
+                                  val context: WaterFallActivity,
+                                  val typeOfFilter: String,
+                                  val totalItemCount: Int) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    class WaterfallFilterTableViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        val waterfall_type_item = itemView.findViewById<TextView>(R.id.waterfall_type_item)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.lf_item_waterfall_type, parent, false)
+
+
+        return WaterfallFilterTableViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val viewHolder = holder as WaterfallFilterTableViewHolder
+
+        when (typeOfFilter) {
+            "all" -> {
+                viewHolder.waterfall_type_item.apply {
+                    text = Utils.getFilter(position + 1)
+                }
+
+                viewHolder.itemView.setOnClickListener {
+                    when (position + 1) {
+                        1 -> {
+
+                        }
+
+                        2 -> {
+                        }
+                    }
+
+
+                }
+            }
+            "time" -> {
+                viewHolder.waterfall_type_item.apply {
+                    text = Utils.getDetailFilterOfTime(position + 1)
+                }
+
+                viewHolder.itemView.setOnClickListener {
+                    waterfallActivity.waterfall_type_blue.visibility = View.GONE
+                    waterfallActivity.waterfall_type_grey.visibility = View.VISIBLE
+                    waterfallActivity.window.dismiss()
+                }
+            }
+            "place" -> {
+                viewHolder.waterfall_type_item.apply {
+                    text = Utils.getDetailFilterOfPlace(position + 1)
+                }
+
+                viewHolder.itemView.setOnClickListener {
+                    waterfallActivity.waterfall_type_blue.visibility = View.GONE
+                    waterfallActivity.waterfall_type_grey.visibility = View.VISIBLE
+                    waterfallActivity.window.dismiss()
+                }
+            }
+            else -> {
+            }
+        }
+    }
+
+    override fun getItemCount(): Int = totalItemCount
+}
