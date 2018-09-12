fd1c12c3ca6ef47a75e03b60aef600c5cf506fbf
WingedVampires <lsc990312@tju.edu.cn>
2018-09-12 10:52:12 +0800

    写完了adapter

--- WePeiYang/tjulibrary/src/main/java/com/twtstudio/retrox/tjulibrary/view/DetialAcitvity.kt
+++ WePeiYang/tjulibrary/src/main/java/com/twtstudio/retrox/tjulibrary/view/DetialAcitvity.kt
@@ -2,12 +2,26 @@ package com.twtstudio.retrox.tjulibrary.view
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.Window
+import com.twt.wepeiyang.commons.experimental.extensions.fitSystemWindowWithStatusBar
+import com.twtstudio.retrox.tjulibrary.R
 
 class DetialAcitvity : AppCompatActivity() {
+
+    lateinit var toolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         val bundle = intent.extras
         val indexOfBook = bundle.getString("indexOfBook")
         super.onCreate(savedInstanceState)
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
+        setContentView(R.layout.activity_detial)
+        toolbar = findViewById<android.support.v7.widget.Toolbar>(R.id.toolbar).also {
+            fitSystemWindowWithStatusBar(it)
+            setSupportActionBar(it)
+        }
+        setSupportActionBar(toolbar)
+        toolbar.title = "图书馆"
     }
 }
