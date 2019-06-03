72d73da3b03ad6c5ef6d20282a9b9600e3009abe
Septieme7 <43516412+Septieme7@users.noreply.github.com>
2019-06-03 22:58:45 +0800

    画好了个人信息界面

---
+++ WePeiYang/theory/src/main/java/com/twt/service/theory/view/Items.kt
@@ -0,0 +1,11 @@
+package com.twt.service.theory.view
+
+import com.twt.wepeiyang.commons.ui.rec.Item
+import com.twt.wepeiyang.commons.ui.rec.ItemController
+
+class ProfileItem() : Item {
+    override val controller: ItemController
+        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+
+
+}
