55394c02901dc3409e951f910624fe7c8b28453d
Ava Rye <14703561608@163.com>
2019-07-06 10:01:04 +0800

    备份 怕了

---
+++ WePeiYang/app/src/main/java/com/twt/service/mall/view/DetailActivity.kt
@@ -0,0 +1,13 @@
+package com.twt.service.mall.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.twt.service.R
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.mall_activity_detail)
+    }
+}
