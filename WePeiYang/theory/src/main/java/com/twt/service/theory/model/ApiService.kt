c588c00c505c3de4ffc47e1c9e9ac1ebe1a19b5b
Yookiely <zihang1998115@126.com>
2019-07-16 15:07:59 +0800

    开始写接口

--- WePeiYang/theory/src/main/java/com/twt/service/theory/model/ApiService.kt
+++ WePeiYang/theory/src/main/java/com/twt/service/theory/model/ApiService.kt
@@ -1,16 +1,28 @@
 package com.twt.service.theory.model
 
+import com.twt.wepeiyang.commons.experimental.network.CommonBody
 import com.twt.wepeiyang.commons.experimental.network.ServiceFactory
+import kotlinx.coroutines.experimental.Deferred
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface TheoryApi{
     //single sign in
-//    @GET("v1/library/login/studentLogin")
-//    // check
-//    @GET("v1/library/login/")
-//
-//
-//    @GET()
+    @GET("v1/theory/exam/getLatestExam")
+    fun getLatestExam(@Query("num") num : Int) : Deferred<CommonBody<String>>
+    // check
+    @GET("v1/theory/notice/getLatestNotcie")
+    fun getLatestNotice(@Query("num") num : Int) : Deferred<CommonBody<String>>
+
+   @GET("v1/theory/login/testLogin")
+   fun testLogin() : Deferred<CommonBody<String>>
+
+   @GET("v1/theory/login/loginStatus")
+   fun loginStatus() : Deferred<CommonBody<String>>
+
+   @GET("v1/theory/user/getUserExam")
+   fun getUserExam(@Query("user_id") userid : Int) : Deferred<CommonBody<String>>
+
 
 
    companion object : TheoryApi by ServiceFactory()
