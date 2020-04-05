078ec05452d5e924336083fc4ed84c3b1b7a7db2
Desirelife <897644126@qq.com>
2020-04-05 16:30:39 +0800

    Parse Gpa html

--- WePeiYang/gpa2/src/main/java/xyz/rickygao/gpa2/spider/GpaSpider.kt
+++ WePeiYang/gpa2/src/main/java/xyz/rickygao/gpa2/spider/GpaSpider.kt
@@ -4,13 +4,12 @@ import android.util.Log
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
-import okhttp3.Call
-import okhttp3.Callback
 import okhttp3.Request
-import okhttp3.Response
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
 import xyz.rickygao.gpa2.spider.cookie.OkHttpClientGenerator
 import xyz.rickygao.gpa2.spider.utils.Classes
-import java.io.IOException
 
 class GpaSpider {
     private fun login(tjuUName: String, tjuPassword: String) {
@@ -20,7 +19,6 @@ class GpaSpider {
     }
 
     fun getGpa(tjuUName: String, tjuPassword: String, semesterId: String): Deferred<String?> {
-
         login(tjuUName, tjuPassword)
         val okHttpClient = OkHttpClientGenerator.generate().build()
         val request = Request.Builder()
@@ -31,4 +29,37 @@ class GpaSpider {
             okHttpClient.newCall(request).execute().body()?.string()
         }
     }
+
+    fun parseHtml(tjuUName: String, tjuPassword: String, semesterId: String) {
+        val htmlStr: String = getGpa(tjuUName, tjuPassword, semesterId).toString()
+
+        val doc = Jsoup.parse(htmlStr)
+        // 根据id获取table
+        val table: Elements = doc.getElementsByClass("gridtable")
+        val tableBody: Elements = table.select("tbody")
+
+
+        // 使用选择器选择该table内所有的<tr> <tr/>
+        val trs: Elements = tableBody.select("tr")
+        //遍历该表格内的所有的<tr> <tr/>
+        for (tr: Element in trs) {
+            // 获取一个tr
+            // 获取该行的所有td节点
+            val tds: Elements = tr.select("td")
+            // 选择某一个td节点
+            for (td: Element in tds) {
+                val text: String = td.text()
+                Log.d("gpaInfo", text)
+            }
+        }
+    }
+
+    data class GpaBean(
+            val semester: String,
+            val courseCode: String,
+            val courseName: String,
+            val courseCredits: Int,
+            val courseScore: Int,
+            val courseGpa: Double
+    )
 }
