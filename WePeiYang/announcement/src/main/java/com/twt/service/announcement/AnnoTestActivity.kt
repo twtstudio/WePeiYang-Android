42f0363f12fecfb1b605ab696f0c11b437d942e9
TranceD <33374756+kaqsice@users.noreply.github.com>
2020-09-15 22:11:31 +0800

    [add] 添加了简单的问题详情，使用假数据

--- WePeiYang/announcement/src/main/java/com/twt/service/announcement/AnnoTestActivity.kt
+++ WePeiYang/announcement/src/main/java/com/twt/service/announcement/AnnoTestActivity.kt
@@ -1,11 +1,19 @@
 package com.twt.service.announcement
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import com.twt.service.announcement.detail.DetailActivity
 
 class AnnoTestActivity : AppCompatActivity() {
+    private lateinit var detailButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_anno_test)
+        detailButton = findViewById(R.id.annoDetailButton)
+        detailButton.setOnClickListener {
+            startActivity(Intent(this, DetailActivity::class.java))
+        }
     }
 }
