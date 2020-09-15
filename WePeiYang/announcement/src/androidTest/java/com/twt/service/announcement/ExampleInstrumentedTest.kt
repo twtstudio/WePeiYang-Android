d9aeaff681aaf9eb0686b053d05c659456c7c8d9
TranceD <33374756+kaqsice@users.noreply.github.com>
2020-09-15 11:37:42 +0800

    [add] 开通了"校务专区"模块(空)

--- WePeiYang/announcement/src/androidTest/java/com/twt/service/announcement/ExampleInstrumentedTest.kt
+++ WePeiYang/announcement/src/androidTest/java/com/twt/service/announcement/ExampleInstrumentedTest.kt
@@ -2,9 +2,11 @@ package com.twt.service.announcement
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
-import org.junit.Assert.assertEquals
+
 import org.junit.Test
 import org.junit.runner.RunWith
+
+import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
